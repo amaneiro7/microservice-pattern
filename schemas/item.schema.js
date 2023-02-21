a6ea2b111a4913c4ec66d4ba244af30417a376f2
Andres Maneiro <amaneiro7@gmail.com>
2023-02-21 17:18:11 -0400

    [UPDATE] schemas item

--- schemas/item.schema.js
+++ schemas/item.schema.js
@@ -3,16 +3,22 @@ const Joi = require('joi');
 const id = Joi.number().integer();
 const serial = Joi.string();
 const activo = Joi.string();
+const categoryId = Joi.number().integer();
+const branchId = Joi.number().integer();
 
 
 const createItemSchema = Joi.object({
   serial: serial,
   activo: activo,
+  categoryId: categoryId.required(),
+  branchId: branchId.required()
 });
 
 const updateItemSchema = Joi.object({
   serial,
-  activo
+  activo,
+  categoryId,
+  branchId
 });
 
 const getItemSchema = Joi.object({
