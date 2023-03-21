619c71cfcb00991131deedb74fcd07cdcdb55afe
Andres Maneiro <amaneiro7@gmail.com>
2023-03-21 16:49:35 -0400

    creacion de columna de equipos obsoletos

--- db/migrations/20230321122454-addcolumn-obsolte-items.js
+++ db/migrations/20230321122454-addcolumn-obsolte-items.js
@@ -3,14 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('item', 'status', {
+    await queryInterface.addColumn('item', 'obsolete', {
       allowNull: false,
       type: Sequelize.BOOLEAN,
-      defaultValue: true
+      defaultValue: false
     })
   },
 
-  async down (queryInterface) {
-    await queryInterface.removeColumn('item', 'status')
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('item', 'obsolete')
   }
 };
