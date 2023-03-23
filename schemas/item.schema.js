effffe2e5570a49d232b4fe5c103a53365a25485
Andres Maneiro <amaneiro7@gmail.com>
2023-03-23 14:41:46 -0400

    checkpoint

--- schemas/item.schema.js
+++ schemas/item.schema.js
@@ -4,6 +4,7 @@ const id = Joi.number().integer();
 const serial = Joi.string().allow(null).allow('').optional();
 const activo = Joi.string().allow(null).allow('').optional();
 const status = Joi.boolean();
+const obsolete = Joi.boolean();
 const categoryId = Joi.number().integer();
 const brandId = Joi.number().integer();
 const modelId = Joi.number().integer();
@@ -12,6 +13,7 @@ const createItemSchema = Joi.object({
   serial: serial,
   activo: activo,
   status: status.required(),
+  obsolete: obsolete.required(),
   categoryId: categoryId.required(),
   brandId: brandId.required(),
   modelId: modelId.required()
@@ -21,6 +23,7 @@ const updateItemSchema = Joi.object({
   serial,
   activo,
   status,
+  obsolete,
   categoryId,
   brandId,
   modelId
